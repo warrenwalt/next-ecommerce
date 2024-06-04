5c98c6e77c8352b84244dfbe280343f21081ac3f
warrenwalt <warrenwalt19@gmail.com>
2024-06-04 18:48:02 +0300

    .

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -27,10 +27,10 @@ const Navbar = () => {
           </Link>
           <div className="hidden xl:flex gap-4">
             <Link href="/">Homepage</Link>
-            <Link href="/">Shop</Link>
+            {/* <Link href="/">Shop</Link>
             <Link href="/">Deals</Link>
             <Link href="/">About</Link>
-            <Link href="/">Contact</Link>
+            <Link href="/">Contact</Link> */}
           </div>
         </div>
         {/* RIGHT */}
